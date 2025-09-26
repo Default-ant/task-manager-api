c579abc4e6f9aaa5a209e4024cbc570202d950ae
Default-ant <krishnamani468@gmail.com>
2025-09-26 21:42:49 +0000

    Initial commit for Render deployment

---
+++ index.js
@@ -0,0 +1,34 @@
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const express = require("express");
+const morgan = require("morgan");
+const { errorHandler, routeNotFound } = require("../functions/middleware/errorMiddleware");
+const routes = require("../functions/routes");
+const dbConnection = require("../functions/utils/connectDB");
+
+dotenv.config();
+dbConnection();
+
+const app = express();
+
+app.use(cors({
+  origin: ["https://mern-task-manager-app.netlify.app", "http://localhost:3000", "http://localhost:3001"],
+  methods: ["GET", "POST", "DELETE", "PUT"],
+  credentials: true,
+}));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+// app.use(morgan("dev"));
+
+app.use("/api", routes);
+app.use(routeNotFound);
+app.use(errorHandler);
+
+// ✅ Cloud Run needs this:
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
---
+++ utils/index.js
@@ -0,0 +1,16 @@
+import jwt from "jsonwebtoken";
+
+const createJWT = (res, userId) => {
+  const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
+    expiresIn: "1d",
+  });
+
+  res.cookie("token", token, {
+  httpOnly: true,
+  secure: false, // ✅ allow over HTTP in dev
+  sameSite: "lax", // ✅ works with HTTP
+  maxAge: 1 * 24 * 60 * 60 * 1000,
+ });
+};
+
+export default createJWT;
