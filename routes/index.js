6e6af4dda6e3e1bd01015e7635d9a020fad26cf2
Default-ant <krishnamani468@gmail.com>
2025-09-28 11:13:10 +0000

    Final CommonJS cleanup

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const router = express.Router();
 
-const taskRoutes = require("./taskRoutes");
-const userRoutes = require("./userRoutes");
-const adminRoutes = require("./adminRoutes");
+const taskRoutes = require("./taskRoute");
+const userRoutes = require("./userRoute");
+
 
 router.use("/task", taskRoutes);
 router.use("/user", userRoutes);
-router.use("/admin", adminRoutes);
+
 
 module.exports = router;
