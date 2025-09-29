f5ac7950ff22925b1dea1ca699d226ef90ab650b
Default-ant <krishnamani468@gmail.com>
2025-09-29 19:00:15 +0000

    cookie check

--- utils/index.js
+++ utils/index.js
@@ -5,13 +5,16 @@ const createJWT = (res, userId) => {
     expiresIn: "7d",
   });
 
+  // ✅ Safe logging here
+  console.log("Setting cookie for user:", userId);
+  console.log("Generated token:", token);
+
   res.cookie("token", token, {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
-    sameSite: "Lax",
-    maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+    sameSite: "None", // 👈 Use "None" for cross-site cookies
+    maxAge: 7 * 24 * 60 * 60 * 1000,
   });
 };
 
-console.log("Cookie set for token:", token);
 module.exports = createJWT;
